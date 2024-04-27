d55de2da808fa3e2651f3bbea309145977a2018c
git\JavierS04 <javiersemper509@gmail.com>
2024-04-27 23:10:00 +0100

    added some interactivity to the webpage and colour added colour to the header and changed how the page is formatted using flex direction, added ui and an area for the boxes to allow for user to have some interactivity to the webpage.

--- scripts.js
+++ scripts.js
@@ -19,3 +19,69 @@ function openInstPage() {
 function openCourses() {
     window.open("https://www.canterbury.ac.uk/study-here/subject-areas/engineering-and-computing", "_blank");
 }
+
+
+//#region this is a section of code to chnage the boxes content 
+const questionBoxes1 = document.getElementById("textContent_1");
+const currentText1 = questionBoxes1.textContent;
+questionBoxes1.addEventListener("mouseover", () => {
+    questionBoxes1.innerText = "this is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes1.addEventListener("mouseleave", () => questionBoxes1.innerText = currentText1);
+
+const questionBoxes2 = document.getElementById("textContent_2");
+const currentText2 = questionBoxes2.textContent;
+questionBoxes2.addEventListener("mouseover", () => {
+    questionBoxes2.innerText = "sjbbad is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes2.addEventListener("mouseleave", () => questionBoxes2.innerText = currentText2);
+
+const questionBoxes3 = document.getElementById("textContent_3");
+const currentText3 = questionBoxes3.textContent;
+questionBoxes3.addEventListener("mouseover", () => {
+    questionBoxes3.innerText = "knsadlkd is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes3.addEventListener("mouseleave", () => questionBoxes3.innerText = currentText3);
+
+const questionBoxes4 = document.getElementById("textContent_4");
+const currentText4 = questionBoxes4.textContent;
+questionBoxes4.addEventListener("mouseover", () => {
+    questionBoxes4.innerText = "xn,cmzb is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes4.addEventListener("mouseleave", () => questionBoxes4.innerText = currentText4);
+
+const questionBoxes5 = document.getElementById("textContent_5");
+const currentText5 = questionBoxes5.textContent;
+questionBoxes5.addEventListener("mouseover", () => {
+    questionBoxes5.innerText = "hsdoai is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes5.addEventListener("mouseleave", () => questionBoxes5.innerText = currentText5);
+
+const questionBoxes6 = document.getElementById("textContent_6");
+const currentText6 = questionBoxes6.textContent;
+questionBoxes6.addEventListener("mouseover", () => {
+    questionBoxes6.innerText = "ioiipuop is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes6.addEventListener("mouseleave", () => questionBoxes6.innerText = currentText6);
+
+const questionBoxes7 = document.getElementById("textContent_7");
+const currentText7 = questionBoxes7.textContent;
+questionBoxes7.addEventListener("mouseover", () => {
+    questionBoxes7.innerText = "qweqewqewq is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes7.addEventListener("mouseleave", () => questionBoxes7.innerText = currentText7);
+
+const questionBoxes8 = document.getElementById("textContent_8");
+const currentText8 = questionBoxes8.textContent;
+questionBoxes8.addEventListener("mouseover", () => {
+    questionBoxes8.innerText = "/.,mnbvcxcvbnm, is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes8.addEventListener("mouseleave", () => questionBoxes8.innerText = currentText8);
+
+const questionBoxes9 = document.getElementById("textContent_9");
+const currentText9 = questionBoxes9.textContent;
+questionBoxes9.addEventListener("mouseover", () => {
+    questionBoxes9.innerText = "1234567ytre is the answer to the previous question that was there is just some random text to take up space and for me to see if the code works as intended";    
+});
+questionBoxes9.addEventListener("mouseleave", () => questionBoxes9.innerText = currentText3);
+//#endregion
