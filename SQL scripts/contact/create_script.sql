6991807c4022f963c58ebac8d6a4a734511461ad
jh1662 <JH1662@canterbury.ac.uk>
2024-05-06 11:51:03 +0100

    again, sorry

--- SQL scripts/contact/create_script.sql
+++ SQL scripts/contact/create_script.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `contacts` (
 	`iD` INT UNSIGNED NOT NULL AUTO_INCREMENT, -- primary key
-	`firstname` TEXT DEFAULT NOT NULL,
-    `lastname` TEXT DEFAULT NOT NULL,
-    `email` TEXT DEFAULT NOT NULL,
-    `phone_number` VARCHAR(32) DEFAULT NOT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
-    `course` TEXT DEFAULT NOT NULL,
-    `question` TEXT DEFAULT NOT NULL,
+	`firstname` TEXT NOT NULL,
+    `lastname` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `phone_number` VARCHAR(32) NOT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
+    `course` TEXT NOT NULL,
+    `question` TEXT NOT NULL,
     PRIMARY KEY (`iD`)
 );
