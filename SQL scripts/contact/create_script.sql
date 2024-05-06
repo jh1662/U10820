032a68f05f9b951ab30d39f9ebca3cba04171a93
jh1662 <JH1662@canterbury.ac.uk>
2024-05-06 11:42:54 +0100

    forgot to declare the primary key ._.

--- SQL scripts/contact/create_script.sql
+++ SQL scripts/contact/create_script.sql
@@ -5,5 +5,6 @@ CREATE TABLE `Course` (
     `email` TEXT DEFAULT NULL,
     `phone_number` VARCHAR(32) DEFAULT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
     `course` TEXT DEFAULT NULL,
-    `question` TEXT DEFAULT NULL
+    `question` TEXT DEFAULT NULL,
+    PRIMARY KEY (`iD`)
 );
