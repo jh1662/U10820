cd6c6254ce4ffc37ab81284764593626aeecd966
jh1662 <JH1662@canterbury.ac.uk>
2024-05-06 11:48:23 +0100

    added 'not null'... why do i have to commit so many times?

--- SQL scripts/contact/create_script.sql
+++ SQL scripts/contact/create_script.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `contacts` (
 	`iD` INT UNSIGNED NOT NULL AUTO_INCREMENT, -- primary key
-	`firstname` TEXT DEFAULT NULL,
-    `lastname` TEXT DEFAULT NULL,
-    `email` TEXT DEFAULT NULL,
-    `phone_number` VARCHAR(32) DEFAULT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
-    `course` TEXT DEFAULT NULL,
-    `question` TEXT DEFAULT NULL,
+	`firstname` TEXT DEFAULT NOT NULL,
+    `lastname` TEXT DEFAULT NOT NULL,
+    `email` TEXT DEFAULT NOT NULL,
+    `phone_number` VARCHAR(32) DEFAULT NOT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
+    `course` TEXT DEFAULT NOT NULL,
+    `question` TEXT DEFAULT NOT NULL,
     PRIMARY KEY (`iD`)
 );
