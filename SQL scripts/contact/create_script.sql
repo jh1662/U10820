7f17e19040101dac0ced3f32b720b188b77e72c7
jh1662 <JH1662@canterbury.ac.uk>
2024-05-06 11:41:37 +0100

    fixed simple syntax mistake and added a comment

--- SQL scripts/contact/create_script.sql
+++ SQL scripts/contact/create_script.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Course` (
 	`firstname` TEXT DEFAULT NULL,
     `lastname` TEXT DEFAULT NULL,
     `email` TEXT DEFAULT NULL,
-    `phone_number` VARCHAR(32) DEFAULT NULL,
+    `phone_number` VARCHAR(32) DEFAULT NULL, -- max phone number is 15 digits but we allow extra characters like spaces, hyphens or plus symbol
     `course` TEXT DEFAULT NULL,
-    `question` TEXT DEFAULT NULL,
+    `question` TEXT DEFAULT NULL
 );
